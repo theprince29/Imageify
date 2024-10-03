98c66f5a4eea25fc46fb1e072ebad6214365608a
Shivam <sg641818@gmail.com>
2024-10-03 21:54:43 +0530

    added type writer effect in search box

--- frontend/src/components/Home/Hero.jsx
+++ frontend/src/components/Home/Hero.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import { Input } from '../ui/input'
+import React, { useState } from 'react'
+import { CiSearch } from "react-icons/ci";
+import { Typewriter } from "react-simple-typewriter";
 
 const Hero = () => {
+  const [searchValue, setSearchValue]= useState("")
   return (
     <div>
         <div className="flex flex-col items-center justify-center min-h-screen bg-white text-center p-8">
@@ -33,13 +35,42 @@ const Hero = () => {
         </p>
 
         {/* Input Box */}
-        <div className="relative max-w-md mx-auto">
-          <input
-            type="text"
-            placeholder="a snowy mountain peak with"
-            className="w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:border-blue-400"
-          />
-        </div>
+        <div className="relative max-w-md mx-auto rounded-lg">
+      <input
+        type="text"
+        placeholder=""
+        className="w-full px-4 py-3 pr-10 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:border-blue-400"
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+      
+      <div className="absolute top-3 left-4 text-gray-400">
+        {
+          !searchValue && (
+
+         
+        <Typewriter
+          words={[
+            "Search mountains...",
+            "Explore beaches...",
+            "Discover cities...",
+          ]}
+          loop={false}
+          cursor
+          cursorStyle="|"
+          typeSpeed={70}
+          deleteSpeed={50}
+          delaySpeed={1000}
+        />
+        )
+      }
+      </div>
+      <div className="absolute right-4 top-4">
+        <CiSearch />
+      </div>
+    </div>
+
+
       </div>
     </div>
     </div>
